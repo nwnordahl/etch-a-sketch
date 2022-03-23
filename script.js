bfa2396a9019c2b174feefd1e143fa2a09829e2c
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-23 23:33:24 +0100

    Make it possible to change grid size

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container");
-const n = 16;
+let n = 16;
 
 for (let i = 0; i < n ** 2; i++) {
   let div = document.createElement("div");
-  div.classList.add("test");
+  div.classList.add("square");
   container.appendChild(div);
 }
 
@@ -15,7 +15,7 @@ function randomColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-const divList = document.querySelectorAll(".test");
+const divList = document.querySelectorAll(".square");
 
 divList.forEach((div) =>
   div.addEventListener("click", (e) => {
@@ -56,3 +56,80 @@ resetButton.addEventListener("click", removeColors);
 let rainbowMode = false;
 const rainbowButton = document.querySelector("#rainbow-button");
 rainbowButton.addEventListener("click", rainbow);
+
+const decrementButton = document.querySelector("#decrement-button");
+const incrementButton = document.querySelector("#increment-button");
+
+incrementButton.addEventListener("click", (e) => {
+  n++;
+  let squares = document.querySelectorAll(".square");
+
+  //   Remove old squares
+  squares.forEach((square) => {
+    container.removeChild(square);
+  });
+
+  for (let i = 0; i < n ** 2; i++) {
+    let div = document.createElement("div");
+    div.style.height = `${90 / n}vh`;
+    div.style.width = `${90 / n}vh`;
+    div.classList.add("square");
+    container.appendChild(div);
+  }
+
+  const divList = document.querySelectorAll(".square");
+
+  divList.forEach((div) =>
+    div.addEventListener("click", (e) => {
+      console.log(e.target.style.backgroundColor);
+      if (
+        e.target.style.backgroundColor &&
+        e.target.style.backgroundColor !== "rgb(246, 237, 232)"
+      ) {
+        e.target.style.backgroundColor = "#f6ede8";
+      } else if (rainbowMode) {
+        e.target.style.backgroundColor = randomColor();
+        return;
+      } else {
+        e.target.style.backgroundColor = "black";
+      }
+    })
+  );
+});
+
+decrementButton.addEventListener("click", (e) => {
+  n--;
+  let squares = document.querySelectorAll(".square");
+
+  //   Remove old squares
+  squares.forEach((square) => {
+    container.removeChild(square);
+  });
+
+  for (let i = 0; i < n ** 2; i++) {
+    let div = document.createElement("div");
+    div.style.height = `${90 / n}vh`;
+    div.style.width = `${90 / n}vh`;
+    div.classList.add("square");
+    container.appendChild(div);
+  }
+
+  const divList = document.querySelectorAll(".square");
+
+  divList.forEach((div) =>
+    div.addEventListener("click", (e) => {
+      console.log(e.target.style.backgroundColor);
+      if (
+        e.target.style.backgroundColor &&
+        e.target.style.backgroundColor !== "rgb(246, 237, 232)"
+      ) {
+        e.target.style.backgroundColor = "#f6ede8";
+      } else if (rainbowMode) {
+        e.target.style.backgroundColor = randomColor();
+        return;
+      } else {
+        e.target.style.backgroundColor = "black";
+      }
+    })
+  );
+});
