e6a410a3685212d3ac4c551d8f58949560d65568
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-27 23:40:43 +0200

    Refactor part 1

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const container = document.querySelector(".container");
-const buttonContainer = document.querySelector(".button-container");
-
-let color = "black";
-const colorPicker = document.querySelector("input[type=color]");
-colorPicker.addEventListener("input", (e) => {
-  color = e.target.value;
-});
-
-const body = document.querySelector("body");
+// Global variables
 let n = 16;
+let mouseClick = false;
+let color = "black";
+let rainbowMode = false;
 
+// Query selectors
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+
+const buttonContainer = document.querySelector(".button-container");
+let resetButton = document.querySelector("#reset-button");
+const decrementButton = document.querySelector("#decrement-button");
+const incrementButton = document.querySelector("#increment-button");
+const rainbowButton = document.querySelector("#rainbow-button");
+const colorPicker = document.querySelector("input[type=color]");
+
+// Initializing grid
 for (let i = 0; i < n ** 2; i++) {
   let div = document.createElement("div");
   div.classList.add("square");
   container.appendChild(div);
+}
+let divList = document.querySelectorAll(".square");
+
+// Helper functions
+function removeColors(e) {
+  divList.forEach((div) => {
+    div.style.backgroundColor = "#f6ede8";
+  });
 }
 
 function randomColor() {
@@ -24,9 +38,21 @@ function randomColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-let divList = document.querySelectorAll(".square");
+function rainbow(e) {
+  if (rainbowMode) {
+    e.target.textContent = "\u{1F308} Off";
+    e.target.style.color = "#4a5096";
+    e.target.style.backgroundColor = "#abc4ff";
+    rainbowMode = false;
+  } else {
+    e.target.textContent = "\u{1F308} On";
+    e.target.style.color = "#edf2fb";
+    e.target.style.backgroundColor = "#80a5ff";
+    rainbowMode = true;
+  }
+}
 
-let mouseClick = false;
+// Event listeners
 container.addEventListener("click", (e) => {
   if (mouseClick) {
     mouseClick = false;
@@ -53,82 +79,7 @@ divList.forEach((div) =>
   })
 );
 
-function removeColors(e) {
-  divList.forEach((div) => {
-    div.style.backgroundColor = "#f6ede8";
-  });
-}
-
-function rainbow(e) {
-  if (rainbowMode) {
-    e.target.textContent = "\u{1F308} Off";
-    e.target.style.color = "#4a5096";
-    e.target.style.backgroundColor = "#abc4ff";
-    rainbowMode = false;
-  } else {
-    e.target.textContent = "\u{1F308} On";
-    e.target.style.color = "#edf2fb";
-    e.target.style.backgroundColor = "#80a5ff";
-    rainbowMode = true;
-  }
-}
-
-let resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", removeColors);
-
-let rainbowMode = false;
-const rainbowButton = document.querySelector("#rainbow-button");
-rainbowButton.addEventListener("click", rainbow);
-
-const decrementButton = document.querySelector("#decrement-button");
-const incrementButton = document.querySelector("#increment-button");
-
-incrementButton.addEventListener("click", (e) => {
-  n++;
-  let squares = document.querySelectorAll(".square");
-
-  //   Remove old squares
-  squares.forEach((square) => {
-    container.removeChild(square);
-  });
-
-  for (let i = 0; i < n ** 2; i++) {
-    let div = document.createElement("div");
-    div.style.height = `${90 / n}vh`;
-    div.style.width = `${90 / n}vh`;
-    div.classList.add("square");
-    container.appendChild(div);
-  }
-
-  divList = document.querySelectorAll(".square");
-
-  divList.forEach((div) =>
-    div.addEventListener("mouseover", (e) => {
-      if (mouseClick) {
-        if (
-          e.target.style.backgroundColor &&
-          e.target.style.backgroundColor !== "rgb(246, 237, 232)"
-        ) {
-          e.target.style.backgroundColor = "#f6ede8";
-        } else if (rainbowMode) {
-          e.target.style.backgroundColor = randomColor();
-          return;
-        } else {
-          e.target.style.backgroundColor = color;
-        }
-      }
-    })
-  );
-
-  buttonContainer.removeChild(resetButton);
-  resetButton = document.createElement("button");
-  resetButton.id = "reset-button";
-  resetButton.textContent = "\u21BA";
-  buttonContainer.prepend(resetButton);
-  resetButton.addEventListener("click", removeColors);
-
-  resetButton.click();
-});
 
 decrementButton.addEventListener("click", (e) => {
   n--;
@@ -201,4 +152,57 @@ saveButton.addEventListener("click", (e) => {
   const dataUrl = canvas.toDataURL("image/png");
   const newTab = window.open("about:blank", "image from canvas");
   newTab.document.write("<img src='" + dataUrl + "' alt='from canvas'/>");
+});
+
+incrementButton.addEventListener("click", (e) => {
+  n++;
+  let squares = document.querySelectorAll(".square");
+
+  //   Remove old squares
+  squares.forEach((square) => {
+    container.removeChild(square);
+  });
+
+  for (let i = 0; i < n ** 2; i++) {
+    let div = document.createElement("div");
+    div.style.height = `${90 / n}vh`;
+    div.style.width = `${90 / n}vh`;
+    div.classList.add("square");
+    container.appendChild(div);
+  }
+
+  divList = document.querySelectorAll(".square");
+
+  divList.forEach((div) =>
+    div.addEventListener("mouseover", (e) => {
+      if (mouseClick) {
+        if (
+          e.target.style.backgroundColor &&
+          e.target.style.backgroundColor !== "rgb(246, 237, 232)"
+        ) {
+          e.target.style.backgroundColor = "#f6ede8";
+        } else if (rainbowMode) {
+          e.target.style.backgroundColor = randomColor();
+          return;
+        } else {
+          e.target.style.backgroundColor = color;
+        }
+      }
+    })
+  );
+
+  buttonContainer.removeChild(resetButton);
+  resetButton = document.createElement("button");
+  resetButton.id = "reset-button";
+  resetButton.textContent = "\u21BA";
+  buttonContainer.prepend(resetButton);
+  resetButton.addEventListener("click", removeColors);
+
+  resetButton.click();
+});
+
+rainbowButton.addEventListener("click", rainbow);
+
+colorPicker.addEventListener("input", (e) => {
+  color = e.target.value;
 });
